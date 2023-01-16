cad42a03b4fd35a21a469b217193fd7a3a6b78a6
Crowdbotics <engineering@crowdbotics.com>
2023-01-16 08:16:32 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/damp_scene_dev_57182/MainActivity.java
@@ -0,0 +1,15 @@
+package com.damp_scene_dev_57182;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "damp_scene_dev_57182";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "damp-scene-dev-57182",
+  url: "https://damp-scene-dev-57182.botics.co",
+  api: "https://damp-scene-dev-57182.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
